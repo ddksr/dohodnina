168e9172eda6ebdf38ad097cdfd2668524bd83c7
Ziga Stopinsek <sigi.kajzer@gmail.com>
2014-06-23 20:16:56 +0200

    javascript file updated

--- js/dohodnina.js
+++ js/dohodnina.js
@@ -1,1 +1,13 @@
+(function ($, _) {
+	var dohodnina = {},
+		lestvice = {};
 
+	$.getJSON('data/lestvice.json', function (data) {
+		lestvice = data;
+	});
+	$.getJSON('data/dohodnina.json', function (data) {
+		dohodnina = data;
+	});
+
+	
+}(jQuery, _));
